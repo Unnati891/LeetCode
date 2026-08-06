b736b7601ad0b850cadc1187fe591acee792ab89
UNNATI KUMARI <unnatimishra891@gmail.com>
2026-08-06 23:35:09 +0530

    Time: 4 ms (41.39%), Space: 175.6 MB (43.16%) - LeetHub

---
+++ 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int[] rearrangeArray(int[] nums) {
+        int pi=0;
+        int ni=1;
+        int ans[]=new int[nums.length];
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]>0){
+                ans[pi]=nums[i];
+                pi+=2;
+            }
+            else{
+                ans[ni]=nums[i];
+                ni+=2;
+            }
+        }
+        return ans;
+    }
+}
